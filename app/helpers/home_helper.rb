5fc9494fb5a72e53b4d28c31baa4bf828d56170d
David Ledford <dcledford@ymail.com>
2012-12-16 22:04:53 -0500

    first commit

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,14 @@
+module HomeHelper
+	def account_login
+		require 'net/imap'
+		imap = Net::IMAP.new('imap.gmail.com')
+
+		imap.login(ENV['email'], ENV['password'])
+
+		if imap.mails.empty?
+			puts "No new mail!"
+		else
+			puts "#{imap.mails.length} messages"
+		end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parsing::Application.config.secret_token = '616d4c9840cf2057d3db3e5c2abc47c84b83116f8e84539a95deb3addaf098afaad01e8cb2104f61ae8b93eedf67f73319928f0f25dcba679d79c6563e02b4df'
