e054b99686fde34f4afbda8faa8f8aeece6fc979
David Ledford <dcledford@ymail.com>
2012-12-17 14:25:33 -0500

    went back to origional working, removed port

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,11 +3,7 @@ module HomeHelper
 		require 'net/pop'
 
 		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
-		#Net::POP3.start('pop.gmail.com', 995, ENV["email"],ENV["auth"]) do |pop|
-    
-		pop = Net::POP3.new('pop.gmail.com', 995)
-
-		pop.start(ENV["email"], ENV["auth"])
+		Net::POP3.start('pop.gmail.com', ENV["email"], ENV["auth"]) do |pop|
 
 		if pop.mails.empty?
 			"No new mail!"
