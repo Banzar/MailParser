b449c3ed5b8e965d420e45b31fa39dbdbf650139
David Ledford <dcledford@ymail.com>
2012-12-16 23:18:15 -0500

    enable ssl added to helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ module HomeHelper
 	def account_login
 		require 'net/pop'
 		pop = Net::POP3.new('pop.gmail.com', 995)
-
+		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
 		pop.start(ENV['email'], ENV['password'])
 
 		if imap.mails.empty?
