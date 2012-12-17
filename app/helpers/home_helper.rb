b1bb680d7edf3a1dd5b3b7ced03f384939e93e84
David Ledford <dcledford@ymail.com>
2012-12-17 16:14:20 -0500

    added for pop.mails.each do |msg|

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,15 +1,20 @@
 module HomeHelper
 	def account_login
-		require 'net/pop'
+	require 'net/pop'
 
 		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
 		Net::POP3.start('pop.gmail.com', 995, ENV["email"], ENV["auth"]) do |pop|
 
-		if pop.mails.empty?
-			"No new mail!"
-		else			
-			"#{pop.mails.length} new mails."
-		end
-	 end   
+				if pop.mails.empty?
+				"No new mail!"
+				else			
+				"#{pop.mails.length} new mails."
+
+				for pop.mails.each do |msg|
+					"#{msg.header}"
+					"#{msg.pop}"
+				end
+			end
+		end   
 	end
 end
