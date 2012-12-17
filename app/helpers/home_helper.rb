23839b29f7bebbd658923b904cddded3a9d8c930
David Ledford <dcledford@ymail.com>
2012-12-16 23:36:30 -0500

    changed password

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
 	def account_login
 		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
-		Net::POP3.start('pop.gmail.com', 995, ENV["email"],Env["password"]) do |pop|  
+		Net::POP3.start('pop.gmail.com', 995, ENV["email"],Env["auth"]) do |pop|  
 		unless pop.mails.empty?
 		     pop.each_mail do |mail|  
 		      email = TMail::Mail.parse(mail.pop)
