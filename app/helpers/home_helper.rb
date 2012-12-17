15a4451a8f9f6eebe0390a1dda08c059eea8d90f
David Ledford <dcledford@ymail.com>
2012-12-16 22:54:47 -0500

    changed to pop from imap

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,14 @@
 module HomeHelper
 	def account_login
-		require 'net/imap'
-		imap = Net::IMAP.new('imap.gmail.com', 993)
+		require 'net/pop'
+		pop = Net::POP3.new('pop.gmail.com', 993)
 
-		imap.login(ENV['email'], ENV['password'])
+		pop.start(ENV['email'], ENV['password'])
 
 		if imap.mails.empty?
 			puts "No new mail!"
 		else
-			puts "#{imap.mails.length} messages"
+			puts "#{pop.mails.length} messages"
 		end
 	end
 end
--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Parsing
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
-     config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
@@ -55,5 +55,8 @@ module Parsing
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+		# SSL Enable
+		config.force_ssl = true
   end
 end
