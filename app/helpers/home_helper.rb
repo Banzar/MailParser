718c148ea9f69c653d2d54e877c812fb14f25244
David Ledford <dcledford@ymail.com>
2012-12-16 23:25:40 -0500

    Changed pop code to get mail

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,14 @@
 module HomeHelper
 	def account_login
-		require 'net/pop'
-		pop = Net::POP3.new('pop.gmail.com', 995)
-		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
-		pop.start(ENV['email'], ENV['password'])
-
-		if imap.mails.empty?
-			puts "No new mail!"
-		else
-			puts "#{pop.mails.length} messages"
-		end
+		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
+		Net::POP3.start('pop.gmail.com', 995, ENV["email"],Env["password"]) do |pop|  
+		unless pop.mails.empty?
+		     pop.each_mail do |mail|  
+		      email = TMail::Mail.parse(mail.pop)
+		      email_obj=EmailedQueries.new
+		      email_obj.save_email(email.from,email.subject,email.body_html)        
+		      end 
+		  end
+	 end   
 	end
 end
