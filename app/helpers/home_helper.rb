d9cb7385cdabfc95f6c6e1a9655508c430f3000e
David Ledford <dcledford@ymail.com>
2012-12-16 23:56:59 -0500

    pop.mails.length to pop.mails.count

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,11 +3,12 @@ module HomeHelper
 		require 'net/pop'
 
 		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
-		Net::POP3.start('pop.gmail.com', 995, ENV["email"],ENV["auth"]) do |pop|    
+		Net::POP3.start('pop.gmail.com', 995, ENV["email"],ENV["auth"]) do |pop|
+    
 		if pop.mails.empty?
 			puts "No new mail!"
 		else
-			puts "#{pop.mails.length} messages"
+			puts "#{pop.mails.count} messages"
 		end
 	 end   
 	end
