75d7996dd6251f72c0128ac86c5e7b92c3b017e2
David Ledford <dcledford@ymail.com>
2012-12-16 23:52:35 -0500

    fixed spelling Env to ENV

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,7 +3,7 @@ module HomeHelper
 		require 'net/pop'
 
 		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
-		Net::POP3.start('pop.gmail.com', 995, ENV["email"],Env["auth"]) do |pop|    
+		Net::POP3.start('pop.gmail.com', 995, ENV["email"],ENV["auth"]) do |pop|    
 		if pop.mails.empty?
 			puts "No new mail!"
 		else
