fff4de7b3fbc71f0b2bc1b16a502c0b51dad66a6
David Ledford <dcledford@ymail.com>
2012-12-16 23:44:20 -0500

    Changed helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,12 @@
 module HomeHelper
 	def account_login
 		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
-		Net::POP3.start('pop.gmail.com', 995, ENV["email"],Env["auth"]) do |pop|  
-		unless pop.mails.empty?
-		     pop.each_mail do |mail|  
-		      email = TMail::Mail.parse(mail.pop)
-		      email_obj=EmailedQueries.new
-		      email_obj.save_email(email.from,email.subject,email.body_html)        
-		      end 
-		  end
+		Net::POP3.start('pop.gmail.com', 995, ENV["email"],Env["auth"]) do |pop|    
+		if pop.mails.empty?
+			puts "No new mail!"
+		else
+			puts "#{imap.mails.length} messages"
+		end
 	 end   
 	end
 end
