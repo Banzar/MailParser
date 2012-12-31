32dc079fc13aeb249de900b19d8b721fecc59402
David Ledford <dcledford@ymail.com>
2012-12-30 23:15:20 -0500

    msg.include?

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,13 +2,13 @@ module HomeHelper
 	def account_login
 	require 'net/pop'
 
-		#Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
-		Net::IMAP.login('pop.gmail.com', 995, ENV["email"], ENV["auth"]) do |pop|
+		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
+		Net::POP3.start('pop.gmail.com', 995, ENV["email"], ENV["auth"]) do |pop|
 			if pop.mails.empty?
 				"There is no new mail!"
 			else
 				pop.mails.each do |msg|
-					if msg.pop.include?('3212165362')
+					if msg.include?('3212165362')
 						msg.pop do |chunk|
 							"#{chunk} \n"
 						end
