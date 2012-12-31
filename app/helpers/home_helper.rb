36cc5f4c3b4c764bdd4e8e1adcaabf625e4284df
David Ledford <dcledford@ymail.com>
2012-12-30 23:07:43 -0500

    changed to IMAP

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,12 +2,12 @@ module HomeHelper
 	def account_login
 	require 'net/pop'
 
-		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
-		Net::POP3.start('pop.gmail.com', 995, ENV["email"], ENV["auth"]) do |pop|
+		#Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)  
+		Net::IMAP.login('pop.gmail.com', 995, ENV["email"], ENV["auth"]) do |pop|
 			if pop.mails.empty?
 				"There is no new mail!"
 			else
-				pop.each_mail do |msg|
+				pop.mails.each do |msg|
 					if msg.pop.include?('3212165362')
 						msg.pop do |chunk|
 							"#{chunk} \n"
